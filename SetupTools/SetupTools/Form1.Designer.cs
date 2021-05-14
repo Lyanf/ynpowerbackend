af458013d6c1fbc7ca96766bd39b30daf4cbbeea
yuxiqian <34335406+yuxiqian@users.noreply.github.com>
2021-05-14 10:23:51 +0800

    [add] 糊了个 SetupTools

---
+++ SetupTools/SetupTools/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿
+namespace SetupTools
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.stepOneInstall = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.stepTwoInstall = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.stepThreeInstall = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.stepFourInstall = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.stepFiveConfigure = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.stepSixInit = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.step2Detect = new System.Windows.Forms.Button();
+            this.stepThreeDetect = new System.Windows.Forms.Button();
+            this.stepFourDetect = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // stepOneInstall
+            // 
+            this.stepOneInstall.Location = new System.Drawing.Point(318, 31);
+            this.stepOneInstall.Name = "stepOneInstall";
+            this.stepOneInstall.Size = new System.Drawing.Size(75, 40);
+            this.stepOneInstall.TabIndex = 0;
+            this.stepOneInstall.Text = "安装";
+            this.stepOneInstall.UseVisualStyleBackColor = true;
+            this.stepOneInstall.Click += new System.EventHandler(this.stepOneInstall_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(35, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "第一步";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label2.Location = new System.Drawing.Point(106, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "安装主预测程序";
+            // 
+            // stepTwoInstall
+            // 
+            this.stepTwoInstall.Location = new System.Drawing.Point(318, 84);
+            this.stepTwoInstall.Name = "stepTwoInstall";
+            this.stepTwoInstall.Size = new System.Drawing.Size(75, 40);
+            this.stepTwoInstall.TabIndex = 0;
+            this.stepTwoInstall.Text = "安装";
+            this.stepTwoInstall.UseVisualStyleBackColor = true;
+            this.stepTwoInstall.Click += new System.EventHandler(this.stepTwoInstall_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label3.Location = new System.Drawing.Point(35, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 18);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "第二步";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label4.Location = new System.Drawing.Point(106, 95);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(206, 18);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "安装 Python 3.7.9 环境";
+            // 
+            // stepThreeInstall
+            // 
+            this.stepThreeInstall.Location = new System.Drawing.Point(318, 137);
+            this.stepThreeInstall.Name = "stepThreeInstall";
+            this.stepThreeInstall.Size = new System.Drawing.Size(75, 40);
+            this.stepThreeInstall.TabIndex = 0;
+            this.stepThreeInstall.Text = "安装";
+            this.stepThreeInstall.UseVisualStyleBackColor = true;
+            this.stepThreeInstall.Click += new System.EventHandler(this.stepThreeInstall_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label5.Location = new System.Drawing.Point(35, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 18);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "第三步";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label6.Location = new System.Drawing.Point(106, 148);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(206, 18);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "安装 PostgreSQL 数据库";
+            // 
+            // stepFourInstall
+            // 
+            this.stepFourInstall.Location = new System.Drawing.Point(318, 190);
+            this.stepFourInstall.Name = "stepFourInstall";
+            this.stepFourInstall.Size = new System.Drawing.Size(75, 40);
+            this.stepFourInstall.TabIndex = 0;
+            this.stepFourInstall.Text = "安装";
+            this.stepFourInstall.UseVisualStyleBackColor = true;
+            this.stepFourInstall.Click += new System.EventHandler(this.stepFourInstall_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label7.Location = new System.Drawing.Point(35, 201);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(65, 18);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "第四步";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label8.Location = new System.Drawing.Point(106, 201);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(206, 18);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "安装 MATLAB 运行时环境";
+            // 
+            // stepFiveConfigure
+            // 
+            this.stepFiveConfigure.Location = new System.Drawing.Point(318, 243);
+            this.stepFiveConfigure.Name = "stepFiveConfigure";
+            this.stepFiveConfigure.Size = new System.Drawing.Size(75, 40);
+            this.stepFiveConfigure.TabIndex = 0;
+            this.stepFiveConfigure.Text = "配置";
+            this.stepFiveConfigure.UseVisualStyleBackColor = true;
+            this.stepFiveConfigure.Click += new System.EventHandler(this.stepFiveConfigure_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label9.Location = new System.Drawing.Point(35, 254);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(65, 18);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "第五步";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label10.Location = new System.Drawing.Point(106, 254);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(116, 18);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "配置环境变量";
+            // 
+            // stepSixInit
+            // 
+            this.stepSixInit.Location = new System.Drawing.Point(318, 296);
+            this.stepSixInit.Name = "stepSixInit";
+            this.stepSixInit.Size = new System.Drawing.Size(75, 40);
+            this.stepSixInit.TabIndex = 0;
+            this.stepSixInit.Text = "初始化";
+            this.stepSixInit.UseVisualStyleBackColor = true;
+            this.stepSixInit.Click += new System.EventHandler(this.stepSixInit_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label11.Location = new System.Drawing.Point(35, 307);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(65, 18);
+            this.label11.TabIndex = 1;
+            this.label11.Text = "第六步";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label12.Location = new System.Drawing.Point(106, 307);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(116, 18);
+            this.label12.TabIndex = 1;
+            this.label12.Text = "初始化数据库";
+            // 
+            // step2Detect
+            // 
+            this.step2Detect.Location = new System.Drawing.Point(406, 84);
+            this.step2Detect.Name = "step2Detect";
+            this.step2Detect.Size = new System.Drawing.Size(75, 40);
+            this.step2Detect.TabIndex = 0;
+            this.step2Detect.Text = "检测";
+            this.step2Detect.UseVisualStyleBackColor = true;
+            this.step2Detect.Click += new System.EventHandler(this.step2Detect_Click);
+            // 
+            // stepThreeDetect
+            // 
+            this.stepThreeDetect.Location = new System.Drawing.Point(406, 137);
+            this.stepThreeDetect.Name = "stepThreeDetect";
+            this.stepThreeDetect.Size = new System.Drawing.Size(75, 40);
+            this.stepThreeDetect.TabIndex = 0;
+            this.stepThreeDetect.Text = "检测";
+            this.stepThreeDetect.UseVisualStyleBackColor = true;
+            this.stepThreeDetect.Click += new System.EventHandler(this.stepThreeDetect_Click);
+            // 
+            // stepFourDetect
+            // 
+            this.stepFourDetect.Location = new System.Drawing.Point(406, 190);
+            this.stepFourDetect.Name = "stepFourDetect";
+            this.stepFourDetect.Size = new System.Drawing.Size(75, 40);
+            this.stepFourDetect.TabIndex = 0;
+            this.stepFourDetect.Text = "检测";
+            this.stepFourDetect.UseVisualStyleBackColor = true;
+            this.stepFourDetect.Click += new System.EventHandler(this.stepFourDetect_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(179, 367);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(163, 40);
+            this.exitButton.TabIndex = 2;
+            this.exitButton.Text = "退出环境配置程序";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 419);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.stepSixInit);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.stepFiveConfigure);
+            this.Controls.Add(this.stepFourDetect);
+            this.Controls.Add(this.stepFourInstall);
+            this.Controls.Add(this.stepThreeDetect);
+            this.Controls.Add(this.stepThreeInstall);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.step2Detect);
+            this.Controls.Add(this.stepTwoInstall);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stepOneInstall);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(537, 475);
+            this.MinimumSize = new System.Drawing.Size(537, 475);
+            this.Name = "Form1";
+            this.Text = "环境配置实用程序";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button stepOneInstall;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button stepTwoInstall;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button stepThreeInstall;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button stepFourInstall;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button stepFiveConfigure;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button stepSixInit;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button step2Detect;
+        private System.Windows.Forms.Button stepThreeDetect;
+        private System.Windows.Forms.Button stepFourDetect;
+        private System.Windows.Forms.Button exitButton;
+    }
+}
+
