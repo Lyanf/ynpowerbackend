7c150b09408973faf31224ed5a1d5c03ac6c375f
yuetsin <akaza_akari@outlook.com>
2021-06-09 15:27:05 +0800

    Update operate.py

--- src/Controller/operate.py
+++ src/Controller/operate.py
@@ -450,7 +450,7 @@ def get_missing_list(start, end, area='yunnan', grain='year', kind='xunhou-souku
 
 def insert_data(array, area='yunnan', grain='year', kind='xunhou-souku-max', superfix8760=False):
     if type(array) == type(None):
-        raise RuntimeError("数据不足，无法完成填充")
+        raise RuntimeError("年度数据不足 8760，无法完成填充")
     print("going to insert data")
     print(array)
     print(len(array))
